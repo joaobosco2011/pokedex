e77b6f9140a7156d00f1a902d216d9e1e19b44fb
João Bosco Junior <joaobosco2011@gmail.com>
2022-04-30 22:56:57 -0300

    Finalizado o css da Navbar
    Feito o estilo do componente Navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Navbar.css'
 
 const Navbar = () => {
     return (
         <>
-            <h1 className="navbar_title">Provu Pokedex</h1>
+            <div className="navbar">
+                <h1 className="navbar_title">Provu Pokedex</h1>
 
-            <ul className="navbar_menu">
-                <li className="navbar_menu_inicio">
-                    <Link to='/component/pokedex'>Início</Link>
-                </li>
-                <li className="navbar_menu_sobre">
-                    <Link to='/component/sobre'>Sobre</Link>
-                </li>
-            </ul>
+                <ul className="navbar_menu">
+                    <li>
+                        <Link className="navbar_menu_inicio" to='/component/pokedex'>Início</Link>
+                    </li>
+                    <li>
+                        <Link className="navbar_menu_sobre" to='/component/sobre'>Sobre</Link>
+                    </li>
+                </ul>
+            </div>
         </>
     )
 }
